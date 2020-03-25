a5dd1c225fd3618741c326da21edfa2dcbe20da3
yjj <yoojj.xyz@gmail.com>
2020-03-26 04:49:03 +0900

    basic

---
+++ python-basic/src/basic/basic.py
@@ -0,0 +1,23 @@
+# 출력
+print('출력')
+
+var = '변수'
+print(var, '출력')
+
+
+
+# 입력 
+text = input('글자 입력 :: ')
+print(text)
+
+num = eval(input('숫자 입력 :: '))
+print(num)
+
+
+
+# 포맷터
+text = '포맷터'
+print('%s 출력' %(text))
+print('{0} 출력' .format(text))
+
+print('{0} {2} {1}'.format('이렇게', '가능', '순서 변경'))
