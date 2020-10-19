3b9d0cd7daa837956bf6c7958ac000b2aa9158fb
yjj <yoojj.xyz@gmail.com>
2020-10-19 11:33:48 +0900

    basic

--- python-basic/src/basic/basic.py
+++ python-basic/src/basic/basic.py
@@ -7,8 +7,8 @@ print(var, '출력')
 
 
 # 입력 
-text = input('글자 입력 :: ')
-print(text)
+string = input('글자 입력 :: ')
+print(string)
 
 num = eval(input('숫자 입력 :: '))
 print(num)
@@ -17,7 +17,8 @@ print(num)
 
 # 포맷터
 text = '포맷터'
-print('%s 출력' %(text))
-print('{0} 출력' .format(text))
 
+print('%s 출력' %(text))        
+print('{0} 출력' .format(text))
 print('{0} {2} {1}'.format('이렇게', '가능', '순서 변경'))
+print(f'{text} 출력')     
