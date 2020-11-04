c682f01cd6fc76aac47686caa824ada34043ad30
yjj <yoojj.xyz@gmail.com>
2020-11-04 15:23:58 +0900

    view test

--- framework-django/django_test/django_app/views.py
+++ framework-django/django_test/django_app/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def get(request, param=None):
+    return Response({
+        'param' : param
+    })
+
+
+@api_view(['POST'])
+def post(request):
+    return Response({
+        'key' : 'value'
+    })
